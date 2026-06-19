d990b7550e2b7617a03ea0d16df025c33b3f4528
Yoel Augustan <yoelagustan@gmail.com>
2026-06-19 09:16:16 +0700

    fix: downgrade .net to version 8 so it is compatible with the deployment server

--- Backend-IKAPRASMUL/IkaPrasmul.Entities/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Backend-IKAPRASMUL/IkaPrasmul.Entities/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace IkaPrasmul.Entities.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.0")
+                .HasAnnotation("ProductVersion", "8.0.23")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
