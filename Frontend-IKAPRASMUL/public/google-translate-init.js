53ba1412fe6aa6000e25c6bd87a5c4161bb0e6fb
Yoel Augustan <yoelagustan@gmail.com>
2026-07-08 12:55:26 +0700

    revert changes

--- Frontend-IKAPRASMUL/public/google-translate-init.js
+++
@@ -1,6 +0,0 @@
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement(
-    { pageLanguage: "id", includedLanguages: "en", autoDisplay: false },
-    "google_translate_element",
-  );
-}
